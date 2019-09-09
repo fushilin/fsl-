f1fdc8070cc70c6b51901268cb2d7a948141d1bc
我演示 <1456191267@qq.com>
2019-09-10 07:16:10 +0800

    2019.09.10 首次

---
+++ 2019.09.10/百思不得姐-01/百思不得姐-01/Class/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐-01
+//
+//  Created by 我演示 on 2019/9/9.
+//  Copyright © 2019 我演示. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2019.09.10/百思不得姐-01/百思不得姐-01/Class/FSLViewController.m
@@ -0,0 +1,33 @@
+//
+//  FSLViewController.m
+//  百思不得姐-01
+//
+//  Created by 我演示 on 2019/9/9.
+//  Copyright © 2019 我演示. All rights reserved.
+//
+
+#import "FSLViewController.h"
+
+@interface FSLViewController ()
+
+@end
+
+@implementation FSLViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = UIColor.redColor;
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
