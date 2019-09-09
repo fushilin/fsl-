b51e6814420b2411aaf335e4d9cdf7ac68e024e4
我演示 <1456191267@qq.com>
2019-09-09 22:30:22 +0800

    第二份代码信息

---
+++ 2019.09.09/百思不得姐-01/百思不得姐-01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  百思不得姐-01
+//
+//  Created by 我演示 on 2019/9/9.
+//  Copyright © 2019 我演示. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
