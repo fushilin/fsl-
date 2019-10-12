7830490e61b10ae63d047d419d141dde1fa21aea
我演示 <1456191267@qq.com>
2019-10-12 17:20:32 +0800

    subviews

---
+++ 2019.10 月ios进阶/2019.10.12/RunTime-MVVM/RunTime-MVVM/Header.h
@@ -0,0 +1,48 @@
+//
+//  Header.h
+//  RunTime-MVVM
+//
+//  Created by 我演示 on 2019/10/12.
+//  Copyright © 2019 我演示. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+
+
+/**
+ 
+ 苹果把常用的字符串定义为const 的信息
+全局变量信息
+ const ： 敞亮
+ const: 当有字符串窗帘的时候，苹果推荐使用const
+ const 与宏的区别
+ 1：编译时刻，宏顶一个，congst 编译时刻
+ 
+ 2： 编译检查 宏不会检查报错  const： 会检查错误
+ 
+ 
+ 
+ */
+
+/**
+ static 作用
+ 1: 修饰局部变量
+  *延长这个局部l变量的生命周期，只要成员运行，局部变量就会一直存在
+  局部l变量智慧分配一次内存
+ 
+ 2： 修饰全局变量
+  只要使用static 修饰的 在程序运行中就已经开始执行
+ 
+  局部变量只会分配一次内存
+ 
+ extension 作用，
+ 1： 声明一个j全局变量，不能定义变量
+ 2： 注意：extension 修饰的变量，不能初始化信息
+ 
+ 
+ */
+
+#endif /* Header_h */
---
+++ 2019.10 月ios进阶/2019.10.12/RunTime-MVVM/RunTime-MVVM/TextViewController.m
@@ -0,0 +1,40 @@
+//
+//  TextViewController.m
+//  RunTime-MVVM
+//
+//  Created by 我演示 on 2019/10/12.
+//  Copyright © 2019 我演示. All rights reserved.
+//
+
+#import "TextViewController.h"
+
+@interface TextViewController ()
+
+@end
+
+@implementation TextViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    void (^blcok) () = ^() {
+    
+    
+}
+    
+//    blc 
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
